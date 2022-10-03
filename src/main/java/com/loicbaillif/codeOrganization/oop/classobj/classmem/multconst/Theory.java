3f862a770bbd97480c253512a3df0a7cbec10180
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-10-03 21:10:02 +0200

    JavaForBeg/multConst/th/v3
    Use-cases for the two constructors

--- src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Theory.java
@@ -5,5 +5,12 @@ import com.loicbaillif.tools.Print;
 public class Theory {
     public static void main() {
         Print.title("Multiple Constructors - Theory", '-', (byte) 70);
+
+        System.out.println("***** 1) Constructor Overloading");
+        Robot robot1 = new Robot();
+        Robot robot2 = new Robot("Andrew", "NDR-114");
+        System.out.println(robot1);
+        System.out.println(robot2);
+
     }
 }
