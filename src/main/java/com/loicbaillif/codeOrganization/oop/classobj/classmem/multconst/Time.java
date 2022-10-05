a6ddf09e4f1cf3b3a4cbd75a4cac6125616d8d54
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-10-05 19:32:16 +0200

    JavaForBeg/multConst/ex1/v1
    First version: Constructors created as expected
    Next: refactor code to have more complete constructors calling less complete ones.

--- src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Time.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Time.java
@@ -4,4 +4,19 @@ public class Time {
     int seconds;
     int minutes;
     int hours;
+
+    Time(int hours) {
+        this.hours = hours;
+    }
+
+    Time(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+    Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
 }
