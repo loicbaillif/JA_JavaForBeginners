31009470b3feaff721b8f62840e79cda25bd499b
loicbaillif <larmedeciel@gmail.com>
2022-10-06 21:05:27 +0200

    JavaForBeg/multConst/ex3/init
    Multiple Constructors - Exercise 3: Phone
    Initialization

---
+++ src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Ex3.java
@@ -0,0 +1,25 @@
+package com.loicbaillif.codeOrganization.oop.classobj.classmem.multconst;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex3 {
+    /* https://hyperskill.org/learn/step/3007
+     * Below is a class named Phone. It has four fields:
+     *          ownerName, countryCode, cityCode and number.
+     *
+     * Add two constructors to the class:
+     *      . The first one takes ownerName and number and initializes the
+     *      corresponding fields;
+     *      . The second one takes ownerName, countryCode, cityCode, number
+     *      and initializes all fields.
+     *
+     * Do not make the fields and constructors private.
+     */
+
+    public static void main() {
+        Print.title("Multiple Constructors - Exercise: Phone");
+
+
+        Print.subtitle("End of exercise");
+    }
+}
---
+++ src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Phone.java
@@ -0,0 +1,8 @@
+package com.loicbaillif.codeOrganization.oop.classobj.classmem.multconst;
+
+public class Phone {
+    String ownerName;
+    String countryCode;
+    String cityCode;
+    String number;
+}
