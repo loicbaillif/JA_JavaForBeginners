a50c2084e3b2a4ceb52e8507d46a761ac63c0cdb
loicbaillif <larmedeciel@gmail.com>
2022-10-06 21:11:52 +0200

    JavaForBeg/multConst/ex3/v2
    Second constructor defined
    Next: define toString(), create some instances, and test both constructors

--- src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Phone.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Phone.java
@@ -12,4 +12,15 @@ public class Phone {
         this.ownerName = ownerName;
         this.number = number;
     }
+
+    public Phone(
+            String ownerName,
+            String countryCode,
+            String cityCode,
+            String number) {
+        this.ownerName = ownerName;
+        this.countryCode = countryCode;
+        this.cityCode = cityCode;
+        this.number = number;
+    }
 }
