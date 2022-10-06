bd8d5c105bdafb346c47184d9a0ec6db180266d4
loicbaillif <larmedeciel@gmail.com>
2022-10-06 21:09:09 +0200

    JavaForBeg/multConst/ex3/v1
    First constructor defined.
    Next: Second constructor

--- src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Phone.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/classobj/classmem/multconst/Phone.java
@@ -1,8 +1,15 @@
 package com.loicbaillif.codeOrganization.oop.classobj.classmem.multconst;
 
 public class Phone {
+    // Attributes
     String ownerName;
     String countryCode;
     String cityCode;
     String number;
+
+    // Constructors
+    public Phone(String ownerName, String number) {
+        this.ownerName = ownerName;
+        this.number = number;
+    }
 }
