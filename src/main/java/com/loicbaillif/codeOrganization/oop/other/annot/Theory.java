879e7467c179ddb7412704f207818889b96b15a8
loicbaillif <larmedeciel@gmail.com>
2022-10-12 21:13:06 +0200

    JavaForBeg/annotations/th/v2
    @Deprecated
    Next: @SuppressWarnings

--- src/main/java/com/loicbaillif/codeOrganization/oop/other/annot/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/other/annot/Theory.java
@@ -10,7 +10,17 @@ public class Theory {
         System.out.println("Line of code starting with @");
         System.out.println("Won't be interpreted, but similar to metadata.");
 
+        Print.subtitle("2) Built-in annotations");
+        System.out.println("There are 3 built-in annotations:");
+        System.out.println("\t- @Deprecated");
+        deprecatedMethod();
+
 
         Print.subtitle("End of theory", '*', (byte) 80);
+    }
+
+    @Deprecated
+    public static void deprecatedMethod() {
+        System.out.println("I am deprecated.");
     }
 }
