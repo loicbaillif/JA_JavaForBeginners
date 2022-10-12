5dea85c1407d9df02a0fdd1b565a62feb20666e2
loicbaillif <larmedeciel@gmail.com>
2022-10-12 20:53:53 +0200

    JavaForBeg/annotations/th/init
    Annotations - Theory: Initialization

---
+++ src/main/java/com/loicbaillif/codeOrganization/oop/other/annot/Main.java
@@ -0,0 +1,7 @@
+package com.loicbaillif.codeOrganization.oop.other.annot;
+
+public class Main {
+    public static void main() {
+        Theory.main();
+    }
+}
---
+++ src/main/java/com/loicbaillif/codeOrganization/oop/other/annot/Theory.java
@@ -0,0 +1,12 @@
+package com.loicbaillif.codeOrganization.oop.other.annot;
+
+import com.loicbaillif.tools.Print;
+
+public class Theory {
+    public static void main() {
+        Print.title("Annotations - Theory");
+
+
+        Print.subtitle("End of theory", '*', (byte) 80);
+    }
+}
