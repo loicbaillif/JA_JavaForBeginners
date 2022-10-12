6e19649d23d97ba339f0d951388f2c3de34b9116
loicbaillif <larmedeciel@gmail.com>
2022-10-12 20:55:02 +0200

    JavaForBeg/annotations/th/v1
    What and where to place annotations

--- src/main/java/com/loicbaillif/codeOrganization/oop/other/annot/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/other/annot/Theory.java
@@ -6,6 +6,10 @@ public class Theory {
     public static void main() {
         Print.title("Annotations - Theory");
 
+        Print.subtitle("1) Annotation, where are you?");
+        System.out.println("Line of code starting with @");
+        System.out.println("Won't be interpreted, but similar to metadata.");
+
 
         Print.subtitle("End of theory", '*', (byte) 80);
     }
