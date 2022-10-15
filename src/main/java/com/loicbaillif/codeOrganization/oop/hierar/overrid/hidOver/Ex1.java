e214b43f74cf784c417f278c1aaa718843e6c0ca
loicbaillif <larmedeciel@gmail.com>
2022-10-15 15:44:58 +0200

    JavaForBeg/hidOver/ex1/init
    hiding and Overriding - Exercise: Shapes area
    Initialization

---
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Ex1.java
@@ -0,0 +1,34 @@
+package com.loicbaillif.codeOrganization.oop.hierar.overrid.hidOver;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex1 {
+    /* https://hyperskill.org/learn/step/3823
+     *
+     * You have five classes: Shape, Triangle, Circle, Square, and
+     * Rectangle. The class Shape has a method area(). This method does
+     * nothing. Override the method in all subclasses. Overridden methods
+     * should return an area of a particular figure. Use class fields for
+     * this.
+     *
+     *      - The area of a triangle is S = b*h / 2
+     *          where h is the height of the triangle,
+     *          b is the base of the triangle.
+     *      - The area of a circle is S = pi * R^2
+     *          where R is the radius of the circle.
+     *          For pi use the Math.PI constant.
+     *      - The area of a square is S = s^2
+     *          where s is the length of the side of the square.
+     *      - The area of a rectangle is S = w * h where w is the width of
+     *          the rectangle and h the height of the rectangle.
+     */
+
+    public static void main() {
+        Print.title("Hiding and Overriding - Exercise: Shapes area");
+
+
+
+        Print.subtitle("End of exercise", '*', (byte) 80);
+
+    }
+}
