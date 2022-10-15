b4d8065fe26ba947dd7fc0c16ae29c852f139950
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-10-15 20:08:34 +0200

    JavaForBeg/hidOver/ex2/v1
    First version, say() method overridden in the three daughter classes

--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Main.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Main.java
@@ -8,3 +8,34 @@ public class Main {
         Ex2.main();
     }
 }
+
+class Animal {
+
+    public void say() {
+        System.out.println("...An incomprehensible sound...");
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    public void say() {
+        System.out.println("meow-meow");
+    }
+}
+
+class Dog2 extends Animal {
+
+    @Override
+    public void say() {
+        System.out.println("arf-arf");
+    }
+}
+
+class Duck extends Animal {
+
+    @Override
+    public void say() {
+        System.out.println("quack-quack");
+    }
+}
