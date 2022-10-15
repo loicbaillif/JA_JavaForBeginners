3e64cd9541d9cea40e931d6a3c180e782b8c5eb7
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-10-15 20:13:29 +0200

    JavaForBeg/hidOver/ex2/v2
    Code moved to dedicated file
    Next: create instances and test

--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Ex2.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Ex2.java
@@ -24,3 +24,30 @@ public class Ex2 {
         Print.subtitle("End of exercise", '*', (byte) 80);
     }
 }
+
+class Animal {
+    public void say() {
+        System.out.println("...An incomprehensible sound...");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void say() {
+        System.out.println("meow-meow");
+    }
+}
+
+class Dog2 extends Animal {
+    @Override
+    public void say() {
+        System.out.println("arf-arf");
+    }
+}
+
+class Duck extends Animal {
+    @Override
+    public void say() {
+        System.out.println("quack-quack");
+    }
+}
--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Main.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Main.java
@@ -8,34 +8,3 @@ public class Main {
         Ex2.main();
     }
 }
-
-class Animal {
-
-    public void say() {
-        System.out.println("...An incomprehensible sound...");
-    }
-}
-
-class Cat extends Animal {
-
-    @Override
-    public void say() {
-        System.out.println("meow-meow");
-    }
-}
-
-class Dog2 extends Animal {
-
-    @Override
-    public void say() {
-        System.out.println("arf-arf");
-    }
-}
-
-class Duck extends Animal {
-
-    @Override
-    public void say() {
-        System.out.println("quack-quack");
-    }
-}
