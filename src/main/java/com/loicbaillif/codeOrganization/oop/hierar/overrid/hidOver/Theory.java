543a98bdbc9b280f880bee1e954c8f19a30dae5b
loicbaillif <larmedeciel@gmail.com>
2022-10-15 12:43:26 +0200

    JavaForBeg/hidOver/th/v1
    Overriding a method: examples

--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Theory.java
@@ -6,8 +6,38 @@ public class Theory {
     public static void main() {
         Print.title("Hiding and Overriding - Theory");
 
-
+        Print.subtitle("1) Overriding instance methods");
+        
 
         Print.subtitle("End of theory", '*', (byte) 80);
+    }
+}
+
+class Mammal {
+    // Attributes
+
+    // Methods
+    public String sayHello() {
+        return "bwofofofofolololalo";
+    }
+}
+
+class Dog extends Mammal {
+    // Attributes
+
+    // Methods
+    @Override
+    public String sayHello() {
+        return "Woof woof!";
+    }
+}
+
+class Human extends Mammal {
+    // Attributes
+
+    // Methods
+    @Override
+    public String sayHello() {
+        return "Good morning.";
     }
 }
