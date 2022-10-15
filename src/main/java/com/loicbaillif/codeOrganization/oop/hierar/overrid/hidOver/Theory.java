116706de66000ada86169d9077b3bf1c297e9bf1
loicbaillif <larmedeciel@gmail.com>
2022-10-15 13:56:16 +0200

    JavaForBeg/hidOver/th/v4
    Forbid overriding

--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/hidOver/Theory.java
@@ -16,6 +16,9 @@ public class Theory {
 
         Print.subtitle("2) Rules for overriding methods");
 
+        Print.subtitle(("3) Forbidding overriding"));
+        System.out.println("Use the keyword _final_ to forbid overriding");
+
 
         Print.subtitle("End of theory", '*', (byte) 80);
     }
@@ -28,6 +31,10 @@ class Mammal {
     public String sayHello() {
         return "bwofofofofolololalo";
     }
+
+    public final String sleep() {
+        return "zzzZZZzzzZZZzzz";
+    }
 }
 
 class Dog extends Mammal {
@@ -38,6 +45,13 @@ class Dog extends Mammal {
     public String sayHello() {
         return "Woof woof!";
     }
+
+    /* following overriding is not allowed
+    @Override
+    public String sleep() {
+        return "woozzzzzwooozzzz";
+    }
+    */
 }
 
 class Human extends Mammal {
