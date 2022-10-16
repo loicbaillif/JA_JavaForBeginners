e5318982372312ad6965261101513943fd5a8659
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-10-16 13:27:15 +0200

    JavaForBeg/overrid/th/v1
    1) Kinds of polymorphism

--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/polym/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/polym/Theory.java
@@ -6,7 +6,8 @@ public class Theory {
     public static void main() {
         Print.title("Polymorphism - Theory");
 
-
+        Print.subtitle("1) Kinds of polymorphism");
+        
 
         Print.subtitle("End of theory", '*', (byte) 80);
     }
