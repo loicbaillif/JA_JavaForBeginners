f11fece81dab5c08d178b6aaccc40857fc610f50
loicbaillif <larmedeciel@gmail.com>
2022-10-16 15:11:44 +0200

    JavaForBeg/overrid/th/v2
    Runtime polymorphic behavior
    Example and instances

--- src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/polym/Theory.java
+++ src/main/java/com/loicbaillif/codeOrganization/oop/hierar/overrid/polym/Theory.java
@@ -7,8 +7,45 @@ public class Theory {
         Print.title("Polymorphism - Theory");
 
         Print.subtitle("1) Kinds of polymorphism");
-        
+
+        Print.subtitle("2) Runtime polymorphic behaviour");
+        MythicAnimal chimera1 = new Chimera();
+        MythicAnimal dragon1 = new Dragon();
+        chimera1.hello();
+        dragon1.hello();
 
         Print.subtitle("End of theory", '*', (byte) 80);
+    }
+}
+
+
+class MythicAnimal {
+    // Attributes
+    private String name = "mythic animal";
+
+    // Methods
+    public void hello() {
+        System.out.println("Hello, I'm sure you don't know me.");
+    }
+}
+
+class Chimera extends MythicAnimal {
+    // Attributes
+    private String name = "chimera";
+
+    // Methods
+    public void hello() {
+        System.out.println("Hello there, 4-in-1 speaking!");
+    }
+}
+
+class Dragon extends MythicAnimal {
+    // Attributes
+    private String name = "dragon";
+
+    // Methods
+    @Override
+    public void hello() {
+        System.out.println("Leave at once or you will be burned.");
     }
 }
