cba17f1b81e949f00cda6b2521909a5a76746b71
loicbaillif <larmedeciel@gmail.com>
2022-11-20 16:28:38 +0100

    #JavaForBeg/ProcStr/ex4/v3 isVowel() function defined

--- src/main/java/com/loicbaillif/basics/strings/procStrings/Ex4.java
+++ src/main/java/com/loicbaillif/basics/strings/procStrings/Ex4.java
@@ -20,14 +20,31 @@ public class Ex4 {
      * characters needed to create a euphonious word from a given word.
      */
 
+    final static String vowels = "aeiouyAEIOUY";
+
     public static void main() {
         Print.title("Processing Strings - Exercise: Euphonious word");
 
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
-        int consecutiveSibling = 0;
-        int result = 0;
+        if (userInput.length() > 2) {
+            int consecutiveSibling = 1;
+            int result = 0;
+            char previousChar = userInput.charAt(0);
+            char currentChar;
+
+
+
+            System.out.println(result);
+        } else {
+            System.out.println(0);
+        }
+
 
         Print.subtitle("End of exercise");
+    }
+
+    public static boolean isVowel(char letter) {
+        return vowels.indexOf(letter) >= 0;
     }
 }
