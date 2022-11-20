684638be5a7914f5af7807416fa06e6fe507ef29
loicbaillif <larmedeciel@gmail.com>
2022-11-20 15:09:06 +0100

    #JavaForBeg/ProcStr/ex4/v0 Create an euphonious word - init

---
+++ src/main/java/com/loicbaillif/basics/strings/procStrings/Ex4.java
@@ -0,0 +1,28 @@
+package com.loicbaillif.basics.strings.procStrings;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex4 {
+    /* https://hyperskill.org/learn/step/3791
+     * All the letters of the English alphabet are divided into
+     * vowels and consonants.
+     *      The vowels are: a, e, i, o, u, y.
+     *      The remaining letters are consonants.
+     *
+     * A word is considered euphonious (pleasant-sounding) if it doesn't
+     * have three or more vowels or consonants in a row. Otherwise, it is
+     * considered discordant (harsh-sounding).
+     *
+     * Your task is to create euphonious words from discordant. You can insert
+     * any letters inside the word. You should output the minimum number of
+     * characters needed to create a euphonious word from a given word.
+     */
+
+    public static void main() {
+        Print.title("Processing Strings - Exercise: Euphonious word");
+
+
+
+        Print.subtitle("End of exercise");
+    }
+}
