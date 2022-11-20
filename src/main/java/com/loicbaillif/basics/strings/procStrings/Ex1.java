de8dfae4886a2c9a7a2104e6e1fbf2e614b82d30
loicbaillif <larmedeciel@gmail.com>
2022-11-20 11:51:36 +0100

    #JavaForBeg/ProcStr/ex1/v1 Get user input

--- src/main/java/com/loicbaillif/basics/strings/procStrings/Ex1.java
+++ src/main/java/com/loicbaillif/basics/strings/procStrings/Ex1.java
@@ -2,6 +2,8 @@ package com.loicbaillif.basics.strings.procStrings;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
+
 public class Ex1 {
     /* https://hyperskill.org/learn/step/2294
      *
@@ -11,6 +13,9 @@ public class Ex1 {
 
     public static void main() {
         Print.title("Processing Strings - Exercise: Double characters");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
 
         Print.subtitle("End of exercise");
     }
