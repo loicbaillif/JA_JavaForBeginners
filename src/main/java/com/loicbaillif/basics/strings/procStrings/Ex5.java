25e0e6857995804c9bf866d9a87a18477e16886b
loicbaillif <larmedeciel@gmail.com>
2022-11-21 07:20:20 +0100

    #JavaForBeg/ProcStr/ex5/v0 Exercise - Lucky ticket: init

---
+++ src/main/java/com/loicbaillif/basics/strings/procStrings/Ex5.java
@@ -0,0 +1,28 @@
+package com.loicbaillif.basics.strings.procStrings;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex5 {
+    /* https://hyperskill.org/learn/step/2281
+     * *** The Lucky Ticket ***
+     *
+     * Paul loves to ride public transport and after receiving a ticket, he
+     * immediately checks whether he got a lucky one. A ticket is considered
+     * a lucky one if the sum of the first three numbers of this ticket
+     * matches the sum of the last three numbers.
+     *
+     * However, Paul does not count well in his head. That is why he asks you
+     * to write a program which will check the equality of the sums and
+     * display "Lucky" if the sums match, and "Regular" if the sums differ.
+     *
+     * A string of six digits is provided as input to the program.
+     */
+
+    public static void main() {
+        Print.title("Processing Strings - Exercise: The lucky ticket");
+
+
+
+        Print.subtitle("End of exercise");
+    }
+}
