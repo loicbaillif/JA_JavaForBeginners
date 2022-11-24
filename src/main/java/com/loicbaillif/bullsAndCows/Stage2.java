ae19c4d3eeecb363c6a22a260ef83cd1255d6652
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-11-24 16:03:28 +0100

    #JavaForBeg/proj2/st2/v5 Tests + analysis moved out of main()

--- src/main/java/com/loicbaillif/bullsAndCows/Stage2.java
+++ src/main/java/com/loicbaillif/bullsAndCows/Stage2.java
@@ -16,21 +16,39 @@ public class Stage2 {
 
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
-        String secretCode = "9305"; // To work with, will be random in final
+        String secret = "9305"; // To work with, will be random in final
+        String formatS = "Number %s has %d bull(s) and %d cow(s).%n";
+
+        // Looking for bulls, then for cows
+        byte[] result = bullsCows(userInput, secret);
+        System.out.printf(formatS, userInput, result[0], result[1]);
+
+        String[] testArray = {"9305", "3059", "3590", "5930", "5039",
+        "9306", "9385", "9505", "1305", "9350", "9035", "5309", "3905",
+        "1293", "5012", "3512", "5129", "1246", "7184", "4862", "2718",
+        "1111", "9999", "9955"};
+
+        for (String proposal: testArray) {
+            result = bullsCows(proposal, secret);
+            System.out.printf(formatS, proposal, result[0], result[1]);
+        }
+
+        Print.subtitle("End of stage 2", '*', (byte) 80);
+    }
+
+
+    public static byte[] bullsCows(String proposal, String solution) {
         byte nbBulls = 0;
         byte nbCows = 0;
 
-        // Looking for bulls
-        for (byte i = 0; i < userInput.length(); i++) {
-            System.out.printf("Loop %d;%n", i);
-            if (Objects.equals(userInput.charAt(i), secretCode.charAt(i))) {
+        for (byte i = 0; i < proposal.length(); i++) {
+            if (Objects.equals(proposal.charAt(i), solution.charAt(i))) {
                 nbBulls++;
+            } else if (solution.indexOf(proposal.charAt(i)) != -1) {
+                nbCows++;
             }
         }
 
-
-
-
-        Print.subtitle("End of stage 2", '*', (byte) 80);
+        return new byte[]{nbBulls, nbCows};
     }
 }
