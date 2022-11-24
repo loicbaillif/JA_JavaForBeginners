ba14238da1c5efbfafb91e9909b5153d6a09cb10
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-11-24 13:35:35 +0100

    #JavaForBeg/proj2/st2/v1 Variables created

--- src/main/java/com/loicbaillif/bullsAndCows/Stage2.java
+++ src/main/java/com/loicbaillif/bullsAndCows/Stage2.java
@@ -2,6 +2,7 @@ package com.loicbaillif.bullsAndCows;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
 
 public class Stage2 {
     /* https://hyperskill.org/projects/53/stages/288/implement
@@ -12,6 +13,11 @@ public class Stage2 {
     public static void main() {
         Print.title("Project: Bulls and Cows - Stage 2: Grader");
 
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        int secretCode = 9305;
+        byte nbBulls = 0;
+        byte nbCows = 0;
 
 
         Print.subtitle("End of stage 2", '*', (byte) 80);
