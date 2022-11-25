5d26d77ec1628ec7adf9f0e26bb89f83ae4d5f2f
loicbaillif <larmedeciel@gmail.com>
2022-11-25 06:13:55 +0100

    #JavaForBeg/ProcStr/ex9/v0 Convert a date - init

---
+++ src/main/java/com/loicbaillif/basics/strings/procStrings/Ex9.java
@@ -0,0 +1,22 @@
+package com.loicbaillif.basics.strings.procStrings;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex9 {
+    /* https://hyperskill.org/learn/step/2296
+     *
+     * *** Convert a date ***
+     * Write a program that takes a date string formatted as YYYY-MM-DD
+     * as input, then converts and outputs it as MM/DD/YYYY.
+     *
+     * For instance, the input 2007-07-21 will result in the following
+     * output 07/21/2007.
+     */
+    public static void main() {
+        Print.title("Processing Strings - Exercise: Convert a date");
+
+
+
+        Print.subtitle("End of exercise", '*', (byte) 80);
+    }
+}
