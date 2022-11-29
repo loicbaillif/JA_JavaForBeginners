6e2abceea1609a9230851dd15ce7f1d4c23353ef
loicbaillif <larmedeciel@gmail.com>
2022-11-29 20:02:55 +0100

    #JavaForBeg/StrBuilder/th/v0 StringBuilder - Init

---
+++ src/main/java/com/loicbaillif/basics/strings/strBuilder/Main.java
@@ -0,0 +1,7 @@
+package com.loicbaillif.basics.strings.strBuilder;
+
+public class Main {
+    public static void main() {
+        Theory.main();
+    }
+}
---
+++ src/main/java/com/loicbaillif/basics/strings/strBuilder/Theory.java
@@ -0,0 +1,13 @@
+package com.loicbaillif.basics.strings.strBuilder;
+
+import com.loicbaillif.tools.Print;
+
+public class Theory {
+    public static void main() {
+        Print.title("StringBuilder");
+
+
+
+        Print.subtitle("End of Theory", '*', (byte) 80);
+    }
+}
