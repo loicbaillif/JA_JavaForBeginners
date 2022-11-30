fcc2b47e815d4b88ae83ed90607445f15d179e0d
loicbaillif <larmedeciel@gmail.com>
2022-11-30 21:46:14 +0100

    #JavaForBeg/StrBuilder/th/v1 Mutable Strings

--- src/main/java/com/loicbaillif/basics/strings/strBuilder/Theory.java
+++ src/main/java/com/loicbaillif/basics/strings/strBuilder/Theory.java
@@ -6,7 +6,12 @@ public class Theory {
     public static void main() {
         Print.title("StringBuilder");
 
-
+        Print.subtitle("1) Mutable Strings");
+        /*
+         * For cases when a lot of String modifications will occur at run time
+         * If we have to perform concatenation in a loop for instance
+         * StringBuilder is a Class
+         */
 
         Print.subtitle("End of Theory", '*', (byte) 80);
     }
