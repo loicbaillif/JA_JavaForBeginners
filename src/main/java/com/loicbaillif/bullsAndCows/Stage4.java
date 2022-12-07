bf55a8e9f402435709cf675e0e3264f458f22d70
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-12-07 21:58:25 +0100

    JavaForBeg/BullsCows/st4/v1 Get random code in main

--- src/main/java/com/loicbaillif/bullsAndCows/Stage4.java
+++ src/main/java/com/loicbaillif/bullsAndCows/Stage4.java
@@ -2,6 +2,8 @@ package com.loicbaillif.bullsAndCows;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
+
 public class Stage4 {
     /* https://hyperskill.org/projects/53/stages/290/
      *
@@ -21,11 +23,68 @@ public class Stage4 {
      * When the game ends, the program should finish its execution.
      */
 
+    // Variables
+    static String nanoTime = genNewNano();
+    static int nanoTimeLength = nanoTime.length();
+    static int currentCodeSize = 0;
+    static int pointerPos = 0; // Pointer in nanoTime String
+    static StringBuilder secretCode = new StringBuilder();
+
+
     public static void main() {
         Print.title("Stage 4/7 : Game Time");
 
-
+        Scanner scanner = new Scanner(System.in);
+        String secretCode = getRandom(scanner);
 
         Print.subtitle("End of Exercise");
+    }
+
+
+    public static String getRandom(Scanner scanner) {
+        int codeSize = scanner.nextInt();
+        if (codeSize > 10 || codeSize < 1) {
+            System.out.printf(
+                    "Error: can't generate a secret number with a " +
+                            "length of %d because there aren't enough unique digits.",
+                    codeSize);
+        } else {
+            while (currentCodeSize < codeSize) {
+                secretCode.append(getDigit());
+                currentCodeSize++;
+            }
+        }
+
+        return secretCode.toString();
+    }
+
+
+    public static String genNewNano() {
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(System.nanoTime()).reverse();
+        return sb1.toString();
+    }
+
+    public static String getDigit() {
+
+        if (currentCodeSize == 0) {
+            // First digit cannot be a 0
+            while (nanoTime.charAt(pointerPos) == '0') {
+                pointerPos++;
+            }
+        }
+
+        String currentDigit = String.valueOf(nanoTime.charAt(pointerPos));
+
+        while (secretCode.indexOf(currentDigit) != -1) {
+            pointerPos++;
+            if (pointerPos >= nanoTimeLength) {
+                nanoTime = genNewNano();
+                pointerPos = 0;
+            }
+            currentDigit = String.valueOf(nanoTime.charAt(pointerPos));
+        }
+
+        return currentDigit;
     }
 }
