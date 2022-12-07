28e1e2808951c2b59e52496fa5c9d06b95a7f654
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-12-07 20:03:35 +0100

    JavaForBeg/StrBuil/ex4/v0 Love of big letters - init

---
+++ src/main/java/com/loicbaillif/basics/strings/strBuilder/Ex4.java
@@ -0,0 +1,22 @@
+package com.loicbaillif.basics.strings.strBuilder;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex4 {
+    /* https://hyperskill.org/learn/daily/23738
+     *
+     * Implement a method that takes a String message as an argument. The
+     * method translates all letters having an even index to uppercase
+     * (index 0 is considered even) and returns the result.
+     *
+     * It's guaranteed that input strings will not be empty.
+     */
+
+    public static void main() {
+        Print.title("StringBuilder - Exercise: Love of big letters");
+
+
+
+        Print.subtitle("End of Exercise", '*', (byte) 80);
+    }
+}
