05cdffc4ec78605e80cb5fc51e669a82324b06a4
loicbaillif <larmedeciel@gmail.com>
2022-12-09 17:30:14 +0100

    JavaForBeg/StrBuil/ex4/v0 Generating passwords - init

---
+++ src/main/java/com/loicbaillif/basics/strings/strBuilder/Ex5.java
@@ -0,0 +1,30 @@
+package com.loicbaillif.basics.strings.strBuilder;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex5 {
+    /* https://hyperskill.org/learn/daily/3789
+     *
+     * *** Generating Passwords ***
+     *
+     * A password is hard to crack if it contains at least A uppercase
+     * letters, at least B lowercase letters, at least C digits and consists
+     * of exactly N symbols. Also, a password cannot contain two or more of
+     * the same characters in a row.
+     *
+     * For the given numbers A, B, C, and N, you should output a password
+     * that matches these requirements.
+     *
+     * It is guaranteed that A, B, C, and N are non-negative integers, and
+     * A + B + C <= N. Keep in mind that any parameter can be equal to zero.
+     * It means that it's ok if the password doesn't contain symbols
+     * of such type.
+     */
+    public static void main() {
+        Print.title("StringBuilder - Exercise: Generating Passwords");
+
+
+
+        Print.subtitle("End of exercise");
+    }
+}
