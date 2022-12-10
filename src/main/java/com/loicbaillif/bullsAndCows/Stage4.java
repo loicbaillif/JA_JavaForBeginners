ac579a97cd4c92ac2f51f9ff711450d79e269a25
loicbaillif <larmedeciel@gmail.com>
2022-12-10 11:34:10 +0100

    JavaForBeg/BullsCows/st4/v1 Ask for length and generate code

--- src/main/java/com/loicbaillif/bullsAndCows/Stage4.java
+++ src/main/java/com/loicbaillif/bullsAndCows/Stage4.java
@@ -2,6 +2,8 @@ package com.loicbaillif.bullsAndCows;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
+
 public class Stage4 {
     /* https://hyperskill.org/projects/53/stages/290/implement
      *
@@ -21,11 +23,80 @@ public class Stage4 {
      * When the game ends, the program should finish its execution.
      */
 
+    // Static variables
+    static String nanoTime = genNewNano();
+    static int nanoTimeLength = nanoTime.length();
+    static int currentCodeSize = 0;
+    static int pointerPos = 0; // Pointer in nanoTime String
+    static StringBuilder secretCode = new StringBuilder();
+
+
+
     public static void main() {
         Print.title("Stage 4: Game Time", '+');
 
+        Scanner scanner = new Scanner(System.in);
+
+        getSecretCode(scanner);
+        System.out.println(secretCode); // DEBUG
 
 
         Print.subtitle("End of Stage 4", '+', (byte) 80);
+    }
+
+
+    public static void getSecretCode(Scanner scanner) {
+        // This method will generate a pseudo-random code, after asking
+        // user/player to provide requested length (1 to 10)
+
+        // Reset variables
+        nanoTime = genNewNano();
+        secretCode = new StringBuilder();
+        currentCodeSize = 0;
+
+        int codeSize = scanner.nextInt();
+        if (codeSize > 10 || codeSize < 1) {
+            System.out.printf(
+                    "Error: can't generate a secret number with a " +
+                            "length of %d because there aren't enough unique digits.",
+                    codeSize);
+        } else {
+            while (currentCodeSize < codeSize) {
+                secretCode.append(getDigit());
+                currentCodeSize++;
+            }
+        }
+    }
+
+
+    public static String genNewNano() {
+        // Generates a new pseudo-random int, based on nanoTime
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(System.nanoTime()).reverse();
+        return sb1.toString();
+    }
+
+
+    public static String getDigit() {
+
+        if (currentCodeSize == 0) {
+            // First digit cannot be a 0
+            while (nanoTime.charAt(pointerPos) == '0') {
+                pointerPos++;
+            }
+        }
+
+        String currentDigit = String.valueOf(nanoTime.charAt(pointerPos));
+
+        while (secretCode.indexOf(currentDigit) != -1) {
+            pointerPos++;
+            if (pointerPos >= nanoTimeLength) {
+                nanoTime = genNewNano();
+                pointerPos = 0;
+            }
+            currentDigit = String.valueOf(nanoTime.charAt(pointerPos));
+        }
+
+        return currentDigit;
     }
 }
