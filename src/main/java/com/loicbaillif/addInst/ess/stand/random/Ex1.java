5b0bd127049fbf038566423545732f0cc22bc04a
loicbaillif <larmedeciel@gmail.com>
2022-12-11 10:31:58 +0100

    JavaForBeg/random/ex1/v0 Find the seed - init

---
+++ src/main/java/com/loicbaillif/addInst/ess/stand/random/Ex1.java
@@ -0,0 +1,20 @@
+package com.loicbaillif.addInst.ess.stand.random;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex1 {
+    /* https://hyperskill.org/learn/step/4940
+     *
+     * Your job is to find the seed between A and B (both inclusive) that
+     * produces N pseudorandom numbers from 0 (inclusive) to K (exclusive).
+     * It should also have the maximum of these N numbers to be the minimum
+     * among all maximums of other seeds in this range.
+     */
+    public static void main() {
+        Print.title("Random - Exercise: Find the seed");
+
+
+
+        Print.subtitle("End of exercise");
+    }
+}
--- src/main/java/com/loicbaillif/addInst/ess/stand/random/Main.java
+++ src/main/java/com/loicbaillif/addInst/ess/stand/random/Main.java
@@ -2,6 +2,8 @@ package com.loicbaillif.addInst.ess.stand.random;
 
 public class Main {
     public static void main() {
-        Theory.main();
+        // Theory.main();
+
+        Ex1.main();
     }
 }
