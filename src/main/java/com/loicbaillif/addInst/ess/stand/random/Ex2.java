19fd62eb8c3bde4198ad78055edf81619429675b
loicbaillif <larmedeciel@gmail.com>
2022-12-12 21:04:25 +0100

    JavaForBeg/random/ex2/v0 Gaussian random numbers - init

---
+++ src/main/java/com/loicbaillif/addInst/ess/stand/random/Ex2.java
@@ -0,0 +1,26 @@
+package com.loicbaillif.addInst.ess.stand.random;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex2 {
+    /* https://hyperskill.org/learn/step/4941
+     *
+     * Gaussian random numbers
+     *
+     * For the given numbers K, N, and M find the first seed that is greater
+     * than or equal to K where each of N Gaussian numbers is less than
+     * or equal to M.
+     *
+     * The input contains numbers K, N, M in a single line.
+     *
+     * You should output the seed.
+     */
+
+    public static void main() {
+        Print.title("Random - Exercise: Gaussian random numbers");
+
+
+
+        Print.subtitle("End of exercise", '*', (byte) 80);
+    }
+}
