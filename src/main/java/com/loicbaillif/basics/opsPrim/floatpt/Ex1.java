71e31d00e39eb8536e7840701401e5b997beecf3
loicbaillif <larmedeciel@gmail.com>
2022-12-14 22:08:51 +0100

    JavaForBeg/float/ex1/v0 Celsius to Fahrenheit

---
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex1.java
@@ -0,0 +1,24 @@
+package com.loicbaillif.basics.opsPrim.floatpt;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex1 {
+    /* https://hyperskill.org/learn/step/2702
+     *
+     * Write a program that reads a temperature in Celsius (°C) and shows
+     * its equivalent in Fahrenheit (°F).
+     *
+     * Use the following formula:
+     *      F = C * 1.8 + 32
+     * where C is a temperature in Celsius and F is the corresponding
+     * temperature in Fahrenheit.
+     */
+    public static void main() {
+        Print.title("Floating - Exercise: Celsius to Fahrenheit",
+                '*', (byte) 70);
+
+
+
+        Print.subtitle("End of exercise");
+    }
+}
--- src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Main.java
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Main.java
@@ -2,6 +2,8 @@ package com.loicbaillif.basics.opsPrim.floatpt;
 
 public class Main {
     public static void main() {
-        Theory.main();
+        // Theory.main();
+
+        Ex1.main();
     }
 }
