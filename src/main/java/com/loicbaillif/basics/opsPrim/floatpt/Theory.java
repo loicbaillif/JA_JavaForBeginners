dbc79373b6050382ff9ac8266653b475822cd983
loicbaillif <larmedeciel@gmail.com>
2022-12-14 20:51:15 +0100

    JavaForBeg/float/th/v1 double: declare and assigne val

--- src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Theory.java
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Theory.java
@@ -6,6 +6,15 @@ public class Theory {
     public static void main() {
         Print.title("Floating-point type: Theory", '+');
 
+        Print.subtitle("1) Declaring and assigning values");
+        double aDouble = 0.0;
+        double bDouble = 1.0;
+        double cDouble = -3.21;
+        double piDouble = 3.14159;
+        double dDouble = 5;
+        System.out.printf("\t%f%n\t%f%n\t%f%n\t%f%n\t%f%n",
+                aDouble, bDouble, cDouble, piDouble, dDouble);
+
 
 
         Print.subtitle("End of theory", '+', (byte) 80);
