4b12c1da8e3492bdb9f1927e6c919df03f2763a4
loicbaillif <larmedeciel@gmail.com>
2022-12-16 19:18:23 +0100

    JavaForBeg/float/ex2/v0 Floating: Distance - init

---
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex2.java
@@ -0,0 +1,19 @@
+package com.loicbaillif.basics.opsPrim.floatpt;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex2 {
+    /* https://hyperskill.org/learn/step/2182
+     *
+     * Write a program that reads the distance between two cities in miles
+     * and the travel time by bus in hours and outputs
+     * the average speed of the bus.
+     */
+    public static void main() {
+        Print.title("Floating - Exercise: Distance");
+
+
+
+        Print.subtitle("End of exercise");
+    }
+}
