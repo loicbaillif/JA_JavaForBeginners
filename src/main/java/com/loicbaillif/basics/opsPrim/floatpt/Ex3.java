2d281cdec6d1ebcb569b2651214735d30e0b795a
loicbaillif <larmedeciel@gmail.com>
2022-12-16 19:25:19 +0100

    JavaForBeg/float/ex3/v0 Evaluate an expression: init

---
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex3.java
@@ -0,0 +1,24 @@
+package com.loicbaillif.basics.opsPrim.floatpt;
+
+import com.loicbaillif.tools.Print;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    /* https://hyperskill.org/learn/step/2705
+     *
+     * *** Evaluate an expression ***
+     *
+     * Write a program that reads four double values a, b, c, d
+     * and then evaluates the following expression
+     *      a * 10.5 + b * 4.4 + (c + d) / 2.2
+     *
+     * The program should output the result.
+     */
+
+    public static void main() {
+        Print.title("Floating - Exercise: Evaluate an expression");
+
+        Print.subtitle("End of exercise");
+    }
+}
