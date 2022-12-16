ea613875c24d3b9a289e7bf7531d9cc10c1ae265
loicbaillif <larmedeciel@gmail.com>
2022-12-16 21:11:01 +0100

    JavaForBeg/float/ex3/v0 Exponential Expression - init

---
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex4.java
@@ -0,0 +1,18 @@
+package com.loicbaillif.basics.opsPrim.floatpt;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex4 {
+    /* https://hyperskill.org/learn/step/2186
+     *
+     * *** Exponential Expression ***
+     *
+     * Write a program which reads a double value x and evaluates the result
+     * of x^3 + x^2 + x + 1
+     */
+    public static void main() {
+        Print.title("Floating - Exercise: Exponential Expression");
+
+        Print.subtitle("End of exercise");
+    }
+}
