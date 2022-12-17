4b5b4d8f1b867a91007f5fca4f3231324a4e7938
loicbaillif <larmedeciel@gmail.com>
2022-12-17 16:06:45 +0100

    JavaForBeg/math/th/v2 Math.min()

--- src/main/java/com/loicbaillif/addInst/ess/stand/math/Theory.java
+++ src/main/java/com/loicbaillif/addInst/ess/stand/math/Theory.java
@@ -7,6 +7,9 @@ public class Theory {
         Print.title("Math library: theory");
 
         Print.subtitle("1) Basic and rounding methods");
+        System.out.println("***** Math.min:");
+        System.out.printf("\tMath.min(14, 42) = %d%n", Math.min(14, 42));
+        System.out.printf("\tMath.min(-13, -37) = %d%n", Math.min(-13, -37));
 
         Print.subtitle("End of theory", '+', (byte) 80);
     }
