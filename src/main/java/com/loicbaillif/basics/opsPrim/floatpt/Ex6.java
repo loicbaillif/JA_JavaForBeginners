55f6fc52cdae6b07650e42cb2bfd92457133145d
loicbaillif <larmedeciel@gmail.com>
2022-12-17 15:39:47 +0100

    JavaForBeg/float/ex6/v0 Difference between two doubles - init

---
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex6.java
@@ -0,0 +1,18 @@
+package com.loicbaillif.basics.opsPrim.floatpt;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex6 {
+    /* https://hyperskill.org/learn/step/2703
+     *
+     * ***** Difference between two doubles *****
+     *
+     * Write a program that reads two double values and prints
+     * the difference between the second and the first one.
+     */
+    public static void main() {
+        Print.title("Floating - Exercise: Difference between two doubles");
+
+        Print.subtitle("End of exercise");
+    }
+}
