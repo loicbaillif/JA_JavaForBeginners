afa2b740cda2c323aae54429378b4b3866a22094
loicbaillif <larmedeciel@gmail.com>
2022-12-17 17:06:42 +0100

    JavaForBeg/math/th/v4 Math.floor()

--- src/main/java/com/loicbaillif/addInst/ess/stand/math/Theory.java
+++ src/main/java/com/loicbaillif/addInst/ess/stand/math/Theory.java
@@ -15,6 +15,10 @@ public class Theory {
         System.out.printf("\tMath.abs(3 - 5) = %d%n", Math.abs(3 - 5));
         System.out.println("\tMath.abs(-12.3) = " + Math.abs(-12.3));
 
+        System.out.println("***** Math.floor():");
+        System.out.println("\tMath.floor(12.3) = " + Math.floor(12.3));
+        System.out.println("\tMath.floor(-3.2) = " + Math.floor(-3.2));
+
         Print.subtitle("End of theory", '+', (byte) 80);
     }
 }
