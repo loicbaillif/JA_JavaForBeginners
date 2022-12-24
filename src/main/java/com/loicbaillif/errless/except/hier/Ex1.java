0de31f7f0e4730532a7e6c5934c978ef75e9a0b5
loicbaillif <larmedeciel@gmail.com>
2022-12-24 17:46:15 +0100

    JavaForBeg/hierExc/ex1/v0 UncheckedException - init

---
+++ src/main/java/com/loicbaillif/errless/except/hier/Ex1.java
@@ -0,0 +1,21 @@
+package com.loicbaillif.errless.except.hier;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex1 {
+    /* https://hyperskill.org/learn/step/16172
+     *
+     * ***** UncheckedException *****
+     *
+     * Create a class named UncheckedException, which must inherit from an
+     * unchecked exception. Inside the UncheckedException class declare
+     * a no-argument constructor.
+     */
+    public static void main() {
+        Print.title("UncheckedException");
+
+
+
+        Print.subtitle("End of exercise", '+', (byte) 80);
+    }
+}
--- src/main/java/com/loicbaillif/errless/except/hier/Main.java
+++ src/main/java/com/loicbaillif/errless/except/hier/Main.java
@@ -2,7 +2,8 @@ package com.loicbaillif.errless.except.hier;
 
 public class Main {
     public static void main() {
-        Theory.main();
+        // Theory.main();
 
+        Ex1.main();
     }
 }
