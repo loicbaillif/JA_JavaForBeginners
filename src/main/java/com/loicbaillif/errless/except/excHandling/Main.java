b792b58fcd559f8fef3b11a8b19b611248ede448
loicbaillif <larmedeciel@gmail.com>
2022-12-25 11:31:07 +0100

    JavaForBeg/excHandl/init Exception Handling - init

---
+++ src/main/java/com/loicbaillif/errless/except/excHandling/Main.java
@@ -0,0 +1,7 @@
+package com.loicbaillif.errless.except.excHandling;
+
+public class Main {
+    public static void main() {
+        Theory.main();
+    }
+}
---
+++ src/main/java/com/loicbaillif/errless/except/excHandling/Theory.java
@@ -0,0 +1,11 @@
+package com.loicbaillif.errless.except.excHandling;
+
+import com.loicbaillif.tools.Print;
+
+public class Theory {
+    public static void main() {
+        Print.title("Theory - Exception Handling", '+');
+
+        Print.subtitle("End of Theory", '+', (byte) 80);
+    }
+}
