b0dd492db2bd7590976642085cd288e008fedb8d
loicbaillif <larmedeciel@gmail.com>
2022-12-27 19:10:10 +0100

    JavaForBeg/excHandl/ex2/v0 BadRequest - init

---
+++ src/main/java/com/loicbaillif/errless/except/excHandling/Ex2.java
@@ -0,0 +1,27 @@
+package com.loicbaillif.errless.except.excHandling;
+
+import com.loicbaillif.tools.Print;
+
+public class Ex2 {
+    /* https://hyperskill.org/learn/daily/16171
+     *
+     * BadRequest Exception
+     *
+     * Create a class named BadRequestException with the constructor that
+     * takes String message as an argument and passes the message to the
+     * superclass. The BadRequestException class must inherit from a
+     * checked exception.
+     */
+    public static void main() {
+        Print.title("Exception Handling - Exercise: String to double");
+
+        Print.subtitle("End of exercise", '+', (byte) 80);
+    }
+}
+
+
+/* TODO
+class BadRequestException extends ... {
+        }
+
+*/
