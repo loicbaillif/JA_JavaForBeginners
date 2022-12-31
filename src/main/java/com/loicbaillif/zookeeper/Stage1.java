7ee35ca8b6b27a03e3b9b8e5ba74d0c654cdf629
loicbaillif <larmedeciel@gmail.com>
2022-12-31 11:51:38 +0100

    JavaForBeg/zookeeper/st1/v2 updated with String block

--- src/main/java/com/loicbaillif/zookeeper/Stage1.java
+++ src/main/java/com/loicbaillif/zookeeper/Stage1.java
@@ -6,7 +6,12 @@ public class Stage1 {
     public static void main() {
         Print.subtitle("Stage 1 - Rush into print");
 
-
+        System.out.println("""
+        I love animals!
+        Let's check on the animals...
+        The deer looks fine.
+        The bat looks happy.
+        The lion looks healthy.""");
 
         Print.subtitle("End of stage 1");
     }
