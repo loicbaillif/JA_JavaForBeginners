7d48e46039333ad02621cfd17ce8e682597904f9
loicbaillif <larmedeciel@gmail.com>
2022-12-31 15:59:21 +0100

    JavaForBeg/textBlocks/th/v2 First examples

--- src/main/java/com/loicbaillif/basics/strings/textBlocks/Theory.java
+++ src/main/java/com/loicbaillif/basics/strings/textBlocks/Theory.java
@@ -6,7 +6,18 @@ public class Theory {
     public static void main() {
         Print.title("Text Blocks");
 
-
+        System.out.println("1) ***** First examples *****");
+        String iliadQuote = "\"But raging still, amidst his navy sat\n" +
+                "The stern Achilles, stedfast in his hate;\n" +
+                "Nor mix’d in combat, nor in council join’d;\n" +
+                "But wasting cares lay heavy on his mind:\n" +
+                "In his black thoughts revenge and slaughter roll,\n" +
+                "And scenes of blood rise dreadful in his soul.\"";
+        String htmlBody = "<html>\n" +
+                "    <body>\n" +
+                "        <p>Hello, World!</p>\n" +
+                "    </body>\n" +
+                "</html>\n";
 
         Print.subtitle("End of Theory", '*', (byte) 80);
     }
