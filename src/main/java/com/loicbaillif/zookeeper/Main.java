b6fbd3c05935ba7cf417d4662d5ea14b77261ec3
loicbaillif <larmedeciel@gmail.com>
2022-12-31 11:46:35 +0100

    JavaForBeg/zookeeper/st1/v0 Stage 1: init

--- src/main/java/com/loicbaillif/zookeeper/Main.java
+++ src/main/java/com/loicbaillif/zookeeper/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main() {
         Print.title("Project Zookeeper");
 
-
+        Stage1.main();
 
         Print.subtitle("End of project", '*', (byte) 80);
     }
---
+++ src/main/java/com/loicbaillif/zookeeper/Stage1.java
@@ -0,0 +1,13 @@
+package com.loicbaillif.zookeeper;
+
+import com.loicbaillif.tools.Print;
+
+public class Stage1 {
+    public static void main() {
+        Print.subtitle("Stage 1 - Rush into print");
+
+
+
+        Print.subtitle("End of stage 1");
+    }
+}
