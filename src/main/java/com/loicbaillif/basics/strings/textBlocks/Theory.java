b5a6c171f353dd1c463ca8fb89bfa8d97ad2cc50
loicbaillif <larmedeciel@gmail.com>
2022-12-31 11:56:33 +0100

    JavaForBeg/textBlocks/init text block - init

---
+++ src/main/java/com/loicbaillif/basics/strings/textBlocks/Theory.java
@@ -0,0 +1,13 @@
+package com.loicbaillif.basics.strings.textBlocks;
+
+import com.loicbaillif.tools.Print;
+
+public class Theory {
+    public static void main() {
+        Print.title("Text Blocks");
+
+
+
+        Print.subtitle("End of Theory", '*', (byte) 80);
+    }
+}
