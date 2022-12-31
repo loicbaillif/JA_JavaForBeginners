360b23a98f280e592fbf30b5d9c56699121a815f
loicbaillif <larmedeciel@gmail.com>
2022-12-31 11:44:52 +0100

    JavaForBeg/zookeeper/init New project: Zookeeper

--- src/main/java/com/loicbaillif/Main.java
+++ src/main/java/com/loicbaillif/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Print.title("JetBrains Academy - Java for beginners");
         // Project stages
-        com.loicbaillif.bullsAndCows.Project1.main();
+        // com.loicbaillif.bullsAndCows.Project1.main();
+        com.loicbaillif.zookeeper.Main.main();
 
         /* Previous chapters */
         // com.loicbaillif.codeOrganization.methods.overloading.Main.main();
---
+++ src/main/java/com/loicbaillif/zookeeper/Main.java
@@ -0,0 +1,13 @@
+package com.loicbaillif.zookeeper;
+
+import com.loicbaillif.tools.Print;
+
+public class Main {
+    public static void main() {
+        Print.title("Project Zookeeper");
+
+
+
+        Print.subtitle("End of project", '*', (byte) 80);
+    }
+}
