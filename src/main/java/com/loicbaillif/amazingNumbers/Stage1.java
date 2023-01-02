1314dbf608f4bd33b304bf2d80a8703f6096668c
loicbaillif <larmedeciel@gmail.com>
2023-01-02 21:14:17 +0100

    JavaForBeg/amazNum/st1/v3 Variables in user input method

--- src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
@@ -2,6 +2,8 @@ package com.loicbaillif.amazingNumbers;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
+
 public class Stage1 {
     /* https://hyperskill.org/projects/184/stages/931/implement
      *
@@ -14,12 +16,15 @@ public class Stage1 {
     public static void main() {
         Print.subtitle("Stage 1 - Buzz Numbers", '*', (byte) 80);
 
-
+        int userInput = getNatural();
 
         Print.subtitle("End of Stage 1", '*', (byte) 80);
     }
 
     private static int getNatural() {
+        // Variables
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getNatural);
 
         return 0;
     }
