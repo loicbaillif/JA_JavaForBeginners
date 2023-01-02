0bf520d5a78a5a98266ae893edd68df9c10dfb83
loicbaillif <larmedeciel@gmail.com>
2023-01-02 20:58:28 +0100

    JavaForBeg/amazNum/st1/v0 Stage 1 - Buzz Numbers

--- src/main/java/com/loicbaillif/amazingNumbers/Main.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Main.java
@@ -2,5 +2,6 @@ package com.loicbaillif.amazingNumbers;
 
 public class Main {
     public static void main() {
+        Stage1.main();
     }
 }
---
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
@@ -0,0 +1,17 @@
+package com.loicbaillif.amazingNumbers;
+
+import com.loicbaillif.tools.Print;
+
+public class Stage1 {
+    /* https://hyperskill.org/projects/184/stages/931/implement
+     *
+     * Project Amazing Numbers - Stage 1
+     */
+    public static void main() {
+        Print.subtitle("Stage 1", '*', (byte) 80);
+
+
+
+        Print.subtitle("End of Stage 1", '*', (byte) 80);
+    }
+}
