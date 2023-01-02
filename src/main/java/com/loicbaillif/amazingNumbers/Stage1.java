de367aef6b873ca75d99bcd6f456868c5bc3118f
loicbaillif <larmedeciel@gmail.com>
2023-01-02 22:09:50 +0100

    JavaForBeg/amazNum/st1/v14 method created to check if number is multiple of 7

--- src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
@@ -32,6 +32,7 @@ public class Stage1 {
         for (int userInput: testValues) {
             System.out.printf("Number %d:%n\t. ", userInput);
             checkParity(userInput);
+            checkBuzz(userInput);
         }
         // END OF TEST PHASE
 
@@ -46,8 +47,11 @@ public class Stage1 {
         String buzzReason3 = "%d is divisible by 7 and ends with 7";
         String notBuzz = "%d is neither divisible by 7 nor does it end with 7";
         String[] buzzStatus = {notBuzz, buzzReason1, buzzReason2, buzzReason3};
+        int userInputStatus = 0;
 
-
+        // Processing
+        userInputStatus += multipleOf7(userInput);
+        System.out.println(userInputStatus);
     }
 
 
@@ -55,6 +59,18 @@ public class Stage1 {
         System.out.printf(checkParity, userInput % 2 == 0 ? "Even" : "Odd");
     }
 
+
+    private static int multipleOf7(int userInput) {
+        // This method returns 0 if userInput is not divisible by 7,
+        // and returns 1 elsewhere;
+
+        // Variables
+        int lastDigit = userInput % 10;
+        int prefix = userInput / 10;
+
+
+        return lastDigit;
+    }
     private static int getNatural() {
         // Variables
         Scanner scanner = new Scanner(System.in);
