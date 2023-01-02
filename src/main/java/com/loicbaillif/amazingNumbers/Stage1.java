d4462b136ea17422277338e519763c9e6a935e26
loicbaillif <larmedeciel@gmail.com>
2023-01-02 21:13:08 +0100

    JavaForBeg/amazNum/st1/v2 define method to return user input

--- src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage1.java
@@ -7,11 +7,20 @@ public class Stage1 {
      *
      * Project Amazing Numbers - Stage 1
      */
+
+    // Static variables
+    static String getNatural = "Enter a natural number:";
+
     public static void main() {
-        Print.subtitle("Stage 1", '*', (byte) 80);
+        Print.subtitle("Stage 1 - Buzz Numbers", '*', (byte) 80);
 
 
 
         Print.subtitle("End of Stage 1", '*', (byte) 80);
+    }
+
+    private static int getNatural() {
+
+        return 0;
     }
 }
