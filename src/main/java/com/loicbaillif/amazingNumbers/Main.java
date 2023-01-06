af02dd5b86b2a66f0f3826b6fda127f3638fc4af
loicbaillif <larmedeciel@gmail.com>
2023-01-06 08:05:09 +0100

    avaForBeg/amazNum/st2/v1 Stage 2: Duck numbers - init bis

--- src/main/java/com/loicbaillif/amazingNumbers/Main.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Main.java
@@ -1,7 +1,10 @@
 package com.loicbaillif.amazingNumbers;
 
+import com.loicbaillif.tools.Print;
+
 public class Main {
     public static void main() {
+        Print.title("PROJECT - Amazing Numbers");
         // Stage1.main();
         Stage2.main();
     }
