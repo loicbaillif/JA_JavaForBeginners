6bea745e91cda894cab4c39e86c0e687c9dbc8b6
loicbaillif <larmedeciel@gmail.com>
2023-01-07 19:22:20 +0100

    avaForBeg/amazNum/st3/v10 setBuzz defined

--- src/main/java/com/loicbaillif/amazingNumbers/ProjectNumber.java
+++ src/main/java/com/loicbaillif/amazingNumbers/ProjectNumber.java
@@ -22,7 +22,6 @@ class ProjectNumber {
     }
 
     // Getters and Setters
-
     boolean isEven() {
         return even;
     }
@@ -30,10 +29,35 @@ class ProjectNumber {
         return odd;
     }
 
+    private void setBuzz() {
+        this.buzz = endsByX(numberValue, 7) || multipleOf7(numberValue);
+    }
     private void setEven() {
         this.even = (numberValue % 2 == 0);
     }
     private void setOdd() {
         this.odd = (numberValue % 2 == 1);
+    }
+
+
+    // Methods
+    private static boolean endsByX(long userInput, int digit) {
+        // This method returns true if userInput ends with digit X,
+        // and returns false elsewhere;
+
+        return (userInput % 10 == digit);
+    }
+    private static boolean multipleOf7(long userInput) {
+        // This method returns false if userInput is not divisible by 7,
+        // and returns true elsewhere;
+        if (userInput < 10) {
+            return (userInput == 0 || userInput == 7);
+        } else {
+            // Variables
+            int lastDigit = (int) (userInput % 10);
+            long prefix = userInput / 10;
+
+            return multipleOf7(Math.abs(prefix - ( 2 * lastDigit)));
+        }
     }
 }
