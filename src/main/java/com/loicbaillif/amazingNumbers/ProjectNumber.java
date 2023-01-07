df3859744753372dca6dd42986a16c33c4aaff88
loicbaillif <larmedeciel@gmail.com>
2023-01-07 18:37:19 +0100

    avaForBeg/amazNum/st3/v3 ProjectNumber created

---
+++ src/main/java/com/loicbaillif/amazingNumbers/ProjectNumber.java
@@ -0,0 +1,10 @@
+package com.loicbaillif.amazingNumbers;
+
+class ProjectNumber {
+    /* ProjectNumber as an object
+     *
+     * Properties:
+     * even, odd, buzz, duck and palindromic.
+     */
+    }
+}
