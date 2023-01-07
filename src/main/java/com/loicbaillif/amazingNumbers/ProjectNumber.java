95a7021622050e7b76c843837a08fae2f7fdc413
loicbaillif <larmedeciel@gmail.com>
2023-01-07 18:48:12 +0100

    avaForBeg/amazNum/st3/v5 Getter and setter for even

--- src/main/java/com/loicbaillif/amazingNumbers/ProjectNumber.java
+++ src/main/java/com/loicbaillif/amazingNumbers/ProjectNumber.java
@@ -6,12 +6,26 @@ class ProjectNumber {
      * Properties:
      * even, odd, buzz, duck and palindromic.
      */
-    public ProjectNumber() {
-        // Properties
-        boolean buzz;
-        boolean duck;
-        boolean even;
-        boolean odd;
-        boolean palindromic;
+
+    // Properties
+    boolean buzz;
+    boolean duck;
+    boolean even;
+    boolean odd;
+    boolean palindromic;
+    int numberValue;
+
+    public ProjectNumber(int numberValue) {
+        this.numberValue = numberValue;
+    }
+
+    // Getters and Setters
+
+    boolean isEven() {
+        return even;
+    }
+
+    private void setEven() {
+        this.even = (numberValue % 2 == 0);
     }
 }
