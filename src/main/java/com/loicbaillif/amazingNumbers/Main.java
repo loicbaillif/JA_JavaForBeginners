9d8a48c56511ee44d8d972dd2c3ae46101d411cb
loicbaillif <larmedeciel@gmail.com>
2023-01-07 18:05:43 +0100

    avaForBeg/amazNum/st3/v0 Stage 3: Palindromic Numbers - init

--- src/main/java/com/loicbaillif/amazingNumbers/Main.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main() {
         Print.title("PROJECT - Amazing Numbers");
         // Stage1.main();
-        Stage2.main();
+        // Stage2.main();
+        Stage3.main();
     }
 }
---
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage3.java
@@ -0,0 +1,17 @@
+package com.loicbaillif.amazingNumbers;
+
+import com.loicbaillif.tools.Print;
+
+public class Stage3 {
+    /* https://hyperskill.org/projects/184/stages/933/implement
+     *
+     * Stage 3 - Palindromic Numbers
+     */
+    public static void main() {
+        Print.subtitle("Stage 3 - Palindromic Numbers", '*', (byte) 80);
+
+
+
+        Print.subtitle("End of Stage 3", '*', (byte) 80);
+    }
+}
