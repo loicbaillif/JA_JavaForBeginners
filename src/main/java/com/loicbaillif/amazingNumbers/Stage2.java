8223f10757e3a56d45d0694dea61d2f455ca8d9a
loicbaillif <larmedeciel@gmail.com>
2023-01-07 14:05:14 +0100

    avaForBeg/amazNum/st2/v2 Request user input

--- src/main/java/com/loicbaillif/amazingNumbers/Stage2.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage2.java
@@ -2,6 +2,9 @@ package com.loicbaillif.amazingNumbers;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Stage2 {
     /* https://hyperskill.org/projects/184/stages/932/implement
      *
@@ -9,12 +12,42 @@ public class Stage2 {
      */
 
     // Static Variables
+    static String errorNotNatural = "Error: This is not a natural number";
+    static String getNatural = "Enter a natural number:";
 
 
     public static void main() {
         Print.subtitle("Stage 2 - Duck Numbers", '*', (byte) 80);
 
+        int userInput = getNatural(); // returns 0 if invalid input
+        if (userInput != 0) {
+            // TODO:
+            // Check parity
+            // Check buzz
+            // Check Duck
+        }
 
         Print.subtitle("End of Stage 2", '*', (byte) 80);
+    }
+
+
+    private static int getNatural() {
+        // Variables
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        System.out.println(getNatural);
+
+        try {
+            userInput = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            userInput = 0;
+        }
+
+        if (userInput <= 0) {
+            System.out.println(errorNotNatural);
+            userInput = 0;
+        }
+
+        return userInput;
     }
 }
