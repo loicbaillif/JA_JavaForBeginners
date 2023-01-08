26860f67086d8b36abb5db8e7ceefa2324b35861
loicbaillif <larmedeciel@gmail.com>
2023-01-08 16:22:13 +0100

    avaForBeg/amazNum/st4/v2 getUserInput() method started; to be developed

--- src/main/java/com/loicbaillif/amazingNumbers/Stage4.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage4.java
@@ -2,6 +2,8 @@ package com.loicbaillif.amazingNumbers;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
+
 public class Stage4 {
     /* https://hyperskill.org/projects/184/stages/934/implement
      *
@@ -19,11 +21,30 @@ public class Stage4 {
             - separate the parameters with one space;
             - enter 0 to exit.
             """;
+    static String askRequest = "Enter a request:";
+    static String errorNotNatural = "The first parameter should be a " +
+            "natural number or zero";
+
+
     public static void main() {
         Print.subtitle("Stage 4 : Gapful Number", '*', (byte) 80);
 
         System.out.println(instructions);
+        getUserInput();
 
         Print.subtitle("End of Stage 4", '*', (byte) 80);
+    }
+
+
+    private static long[] getUserInput() {
+        // Variables
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        System.out.println(askRequest);
+
+        userInput = scanner.next();
+
+        return new long[2];
     }
 }
