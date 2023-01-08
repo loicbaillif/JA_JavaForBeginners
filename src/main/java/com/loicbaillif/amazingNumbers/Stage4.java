7cdec51b4c0e37e04bbf3de993ebd8e8cb5f3d36
loicbaillif <larmedeciel@gmail.com>
2023-01-08 16:03:33 +0100

    avaForBeg/amazNum/st4/v1 Initial instructions

--- src/main/java/com/loicbaillif/amazingNumbers/Stage4.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage4.java
@@ -7,10 +7,22 @@ public class Stage4 {
      *
      * Amazing Numbers - Stage 4 : Gapful number
      */
+
+    // Static Variables
+    static String instructions = """
+            Supported requests:
+            - enter a natural number to know its properties;
+            - enter two natural numbers to obtain the properties of the list:
+              * the first parameter represents a starting number;
+              * the second parameter shows how many consecutive \
+            numbers are to be printed;
+            - separate the parameters with one space;
+            - enter 0 to exit.
+            """;
     public static void main() {
         Print.subtitle("Stage 4 : Gapful Number", '*', (byte) 80);
 
-
+        System.out.println(instructions);
 
         Print.subtitle("End of Stage 4", '*', (byte) 80);
     }
