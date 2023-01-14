6eb474a4660eec3365e212ebce6826f5c14bd3ec
loicbaillif <larmedeciel@gmail.com>
2023-01-14 15:15:42 +0100

    JavaForBeg/Float/ex8/v1 Variables defined

--- src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex8.java
+++ src/main/java/com/loicbaillif/basics/opsPrim/floatpt/Ex8.java
@@ -2,12 +2,17 @@ package com.loicbaillif.basics.opsPrim.floatpt;
 
 import com.loicbaillif.tools.Print;
 
+import java.util.Scanner;
+
 public class Ex8 {
     // https://hyperskill.org/learn/daily/3796
     public static void main() {
         Print.title("Floating-point type - Exercise: Area of a circle");
 
-
+        // Variables
+        Scanner scanner = new Scanner(System.in);
+        float radius = scanner.nextFloat();
+        double area;
 
         Print.subtitle("End of exercise", '*', (byte) 80);
     }
