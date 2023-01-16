bdf64f31e53d39c652a0d3b9bddfa276902642d3
loicbaillif <larmedeciel@gmail.com>
2023-01-16 21:42:06 +0100

    JavaForBeg/amazNum/st5/v1 Static variables updated from stage 4

--- src/main/java/com/loicbaillif/amazingNumbers/Stage5.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage5.java
@@ -8,6 +8,36 @@ public class Stage5 {
      * Stage 5 - Spy Numbers
      */
 
+    // Static Variables
+    static String instructions = """
+            Welcome to Amazing Numbers!
+            
+            Supported requests:
+            - enter a natural number to know its properties;
+            - enter two natural numbers to obtain the properties of the list:
+              * the first parameter represents a starting number;
+              * the second parameter shows how many consecutive \
+            numbers are to be printed;
+            - two naturals numbers and a property to search for;
+            - separate the parameters with one space;
+            - enter 0 to exit.
+            """;
+    static String askRequest = "Enter a request:";
+    static String errorLen = "Error: The second parameter should be " +
+            "a natural number";
+    static String errorNotNatural = "Error: The first parameter should be a " +
+            "natural number or zero";
+    static String farewell = "Thanks for using Amazing Numbers, goodbye!";
+    static String numberStatus = """
+            Properties of %d
+                       even: %b
+                        odd: %b
+                       buzz: %b
+                       duck: %b
+                palindromic: %b
+                     gapful: %b
+                        spy: %b
+            """;
 
     public static void main() {
         Print.subtitle("Stage 5 : Spy Numbers", '*', (byte) 80);
