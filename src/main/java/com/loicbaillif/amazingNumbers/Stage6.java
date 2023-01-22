d0ddc647f07a98f585a1dccaa8d8c1fb0afb58de
loicbaillif <larmedeciel@gmail.com>
2023-01-22 15:40:38 +0100

    JavaForBeg/amazNum/st6/v1 Static variables updated

--- src/main/java/com/loicbaillif/amazingNumbers/Stage6.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage6.java
@@ -8,6 +8,51 @@ public class Stage6 {
      * Stage 6 : Sunny and Square Numbers
      */
 
+    // Static Variables
+    static String instructions = """
+            Welcome to Amazing Numbers!
+            
+            Supported requests:
+            - enter a natural number to know its properties;
+            - enter two natural numbers to obtain the properties of the list:
+              * the first parameter represents a starting number;
+              * the second parameter shows how many consecutive \
+            numbers are to be printed;
+            - two natural numbers and a property to search for;
+            - two natural numbers and two properties to search for;
+            - separate the parameters with one space;
+            - enter 0 to exit.
+            """;
+    static String askRequest = "Enter a request:";
+    static String errorEmpty = "Error: User input cannot be empty.";
+    static String errorInvalidProperty = """
+    Error: the property [%s] is wrong.
+    Available properties: ["BUZZ", "DUCK", "EVEN", "GAPFUL", \
+    "ODD", "PALINDROMIC", "SPY", "SQUARE", "SUNNY"]
+    """;
+    static String errorLen = "Error: The second parameter should be " +
+            "a natural number";
+    static String errorNotNatural = "Error: The first parameter should be a " +
+            "natural number or zero";
+    static String errorNbArgs = "Error: The program needs 1 to 3 arguments";
+    static String farewell = "Thanks for using Amazing Numbers, goodbye!";
+    static String numberStatus = """
+            Properties of %d
+                       even: %b
+                        odd: %b
+                       buzz: %b
+                       duck: %b
+                palindromic: %b
+                     gapful: %b
+                        spy: %b
+                     square: %b
+                      sunny: %b
+            """;
+    static String[] listProperties =
+            {"BUZZ", "DUCK", "EVEN", "GAPFUL", "ODD", "PALINDROMIC", "SPY",
+            "SQUARE", "SUNNY"};
+    static ProjectNumber projectNumber = new ProjectNumber(1);
+
     public static void main() {
         Print.subtitle("Stage 6 : Sunny and Square Numbers", '*', (byte) 80);
 
