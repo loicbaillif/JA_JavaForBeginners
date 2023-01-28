eba8df896313611360355c72740f9a62586b6228
loicbaillif <larmedeciel@gmail.com>
2023-01-28 08:22:37 +0100

    JavaForBeg/amazNum/st7/v2 Welcome and farewell messages added

--- src/main/java/com/loicbaillif/amazingNumbers/Stage7.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage7.java
@@ -8,11 +8,31 @@ public class Stage7 {
      * Stage 7: Jumping Numbers
      */
 
+    // Variables
+    static String farewell = "Thanks for using Amazing Numbers, goodbye!";
+    static String instructions = """
+            Welcome to Amazing Numbers!
+                        
+            Supported requests:
+            - enter a natural number to know its properties;
+            - enter two natural numbers to obtain the properties of the list:
+              * the first parameter represents a starting number;
+              * the second parameter shows how many consecutive numbers are to be processed;
+            - two natural numbers and properties to search for;
+            - separate the parameters with one space;
+            - enter 0 to exit.
+            """;
+
 
     public static void main() {
         Print.subtitle("Stage 7 : Jumping Numbers", '*', (byte) 80);
 
+        // Variables
 
+        System.out.println(instructions);
+
+
+        System.out.println(farewell);
 
         Print.subtitle("End of Stage 7", '*', (byte) 80);
     }
