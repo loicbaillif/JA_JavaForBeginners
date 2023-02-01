c8de8af3e725c43946ad1045d1455faacc3f655f
loicbaillif <larmedeciel@gmail.com>
2023-02-01 07:18:34 +0100

    JavaForBeg/debug/ex2/v3 Log the "after" for empty

--- src/main/java/com/loicbaillif/errless/debug/Ex2.java
+++ src/main/java/com/loicbaillif/errless/debug/Ex2.java
@@ -11,8 +11,12 @@ public class Ex2 {
 
         String ex1 = "string";
         String ex2 = "Capitalized";
+        String ex3 = "";
+        String ex4 = "g";
         System.out.println(capitalize(ex1));
         System.out.println(capitalize(ex2));
+        System.out.println(capitalize(ex3));
+        System.out.println(capitalize(ex4));
 
 
         Print.title(("End of exercise"));
@@ -22,6 +26,7 @@ public class Ex2 {
     public static String capitalize(String str) {
         System.out.printf("Before: %s%n", str); // LOG
         if (str == null || str.isBlank()) {
+            System.out.printf("After: %s%n", str);
             return str;
         }
 
