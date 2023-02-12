bfa607ee37e35082a378b242172791763300eb06
loicbaillif <larmedeciel@gmail.com>
2023-02-12 22:08:27 +0100

    JavaForBeg/amazNum/st8/v27 Global tests started - instructions updated

--- src/main/java/com/loicbaillif/amazingNumbers/Stage8.java
+++ src/main/java/com/loicbaillif/amazingNumbers/Stage8.java
@@ -21,8 +21,9 @@ public class Stage8 {
             - enter a natural number to know its properties;
             - enter two natural numbers to obtain the properties of the list:
               * the first parameter represents a starting number;
-              * the second parameter shows how many consecutive numbers are to be processed;
+              * the second parameter shows how many consecutive numbers are to be printed;
             - two natural numbers and properties to search for;
+            - a property preceded by minus must not be present in numbers;
             - separate the parameters with one space;
             - enter 0 to exit.
             """;
