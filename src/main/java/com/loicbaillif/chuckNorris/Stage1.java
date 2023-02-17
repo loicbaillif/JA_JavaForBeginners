bf24147705d1b5ee8553bc9a9bfb410f1ab9a650
loicbaillif <larmedeciel@gmail.com>
2023-02-17 21:10:49 +0100

    JavaForBeg/chuckNor/st1/v1 Titles

--- src/main/java/com/loicbaillif/chuckNorris/Stage1.java
+++ src/main/java/com/loicbaillif/chuckNorris/Stage1.java
@@ -1,5 +1,7 @@
 package com.loicbaillif.chuckNorris;
 
+import com.loicbaillif.tools.Print;
+
 public class Stage1 {
     /*
      * https://hyperskill.org/projects/293/stages/1536/implement
@@ -7,5 +9,9 @@ public class Stage1 {
      * Project Chuck Norris Cipher Encoder - Stage 1 : Analyze the words
      */
     public static void main() {
+        Print.subtitle("Stage 1 - Analyze the words", '*', (byte) 80);
+
+
+        Print.subtitle("End of Stage 1", '*', (byte) 80);
     }
 }
